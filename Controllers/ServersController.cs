992c0585ae3c9d4056971221d0a820fa8edaa8cf
Jorge Berti <jorgehoodoo@gmail.com>
2021-02-22 20:18:17 -0300

    In-Progress - implementações dos endpoints.

--- Controllers/ServersController.cs
+++ Controllers/ServersController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VideoServerAPI.Data;
+using VideoServerAPI.Dto.Server;
+using VideoServerAPI.DTO.Video;
 using VideoServerAPI.Models;
 
 namespace VideoServerAPI.Controllers
@@ -29,10 +33,10 @@ namespace VideoServerAPI.Controllers
         }
 
         // GET: api/Servers/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Server>> GetServer(Guid id)
+        [HttpGet("{serverId}")]
+        public async Task<ActionResult<Server>> GetServer(Guid serverId)
         {
-            var server = await _context.Servers.FindAsync(id);
+            var server = await _context.Servers.FindAsync(serverId);
 
             if (server == null)
             {
@@ -42,53 +46,173 @@ namespace VideoServerAPI.Controllers
             return server;
         }
 
-        // PUT: api/Servers/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutServer(Guid id, Server server)
+        [HttpGet("{serverId}")]
+        [Route("api/[controller]/available/{id}")]
+        public async Task<ActionResult<string>> IsServerOnline(Guid serverId)
         {
-            if (id != server.Id)
+            string response = "";
+
+            var server = await _context.Servers.FindAsync(serverId);
+            if (server == null)
             {
-                return BadRequest();
+                response = "Not found";
+            }
+            else
+            {
+                var serverIp = server.Ip;
+                var serverPort = server.Port;
+
+                TcpClient telnet = new TcpClient(serverIp, serverPort);
+                NetworkStream telnetStream = telnet.GetStream();
+                string requestResponse = new StreamReader(telnetStream).ToString();
+
+                string prompt = requestResponse.TrimEnd();
+                prompt = requestResponse.Substring(prompt.Length - 1, 1);
+                if (prompt == "$" || prompt == ">")
+                {
+                    response = "Server online";
+                }
+                else
+                {
+                    response = "Server offline or unreachable";
+                }                    
             }
 
-            _context.Entry(server).State = EntityState.Modified;
+            return response;
+        }
 
+        // POST: api/Servers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Server>> PostServer([FromBody]ServerDTO serverDto)
+        {
+            if (ServerExists(serverDto.Ip, serverDto.Port)){
+                return StatusCode(409, "Servidor já existente");
+            }
+
+            // TODO: Load state from previously suspended application
+            Server server = new Server
+            {
+                Id = Guid.NewGuid(),
+                Ip = serverDto.Ip,
+                Name = serverDto.Name,
+                Port = serverDto.Port
+            };
+
+            _context.Servers.Add(server);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetServer", new { id = server.Id }, server);
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/{serverId}/videos")]
+        public async Task<IActionResult> Addvideo(Guid serverId, [FromBody] VideoInputDTO videoDTO)
+        {
+            var server = await _context.Servers.FindAsync(serverId);
+            if (server == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Video video = new Video
+                {
+                    Id = new Guid(),
+                    Description = videoDTO.Description,
+                    VideoContent = Convert.FromBase64String(Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(videoDTO.VideoDataBase64))) 
+                };            
+
+                server.Videos.Add(video); 
+                _context.Entry(server).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;                    
+                }
+            }
+            return NoContent();            
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{serverId}/videos/{videoId}")]
+        public async Task<ActionResult<VideoInformationDTO>> GetVideoInformation(Guid serverId, Guid videoId)
+        {
+            Server server;
+            Video video;
+            VideoInformationDTO videoInfoDTO;
+
+            server = await _context.Servers.FindAsync(serverId);
+            if (server == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                video = await _context.Videos.FindAsync(videoId);
+                if (video == null)
+                {
+                    return NotFound();
+                }
+            }
+
+            videoInfoDTO = new VideoInformationDTO
+            {
+                VideoId = video.Id,
+                Description = video.Description,
+                SizeInBytes = video.VideoContent.Length
+            };
+            
+            return videoInfoDTO;    
+
+        }
+
+        [HttpDelete]
+        [Route("/api/servers/{serverId}/videos/{videoId}")]
+        public async Task<IActionResult> DeleteVideo(Guid serverId, Guid videoId)
+        {
+            Server server;
+            Video video;
+
+            server = await _context.Servers.FindAsync(serverId);
+            if (server == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                video = await _context.Videos.FindAsync(videoId);
+                if (video == null)
+                {
+                    return NotFound();
+                }
+            }
+            server.Videos.Remove(video);
+            _context.Videos.Remove(video);
+            _context.Entry(server).State = EntityState.Modified;
             try
             {
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!ServerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                throw;
             }
 
             return NoContent();
-        }
 
-        // POST: api/Servers
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Server>> PostServer(Server server)
-        {
-            _context.Servers.Add(server);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetServer", new { id = server.Id }, server);
         }
 
         // DELETE: api/Servers/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteServer(Guid id)
+        [HttpDelete("{serverId}")]
+        public async Task<IActionResult> DeleteServer(Guid serverId)
         {
-            var server = await _context.Servers.FindAsync(id);
+            var server = await _context.Servers.FindAsync(serverId);
             if (server == null)
             {
                 return NotFound();
@@ -100,9 +224,9 @@ namespace VideoServerAPI.Controllers
             return NoContent();
         }
 
-        private bool ServerExists(Guid id)
+        private bool ServerExists(string ip, int port)
         {
-            return _context.Servers.Any(e => e.Id == id);
+            return _context.Servers.Any(server => server.Ip == ip && server.Port == port);
         }
     }
 }
